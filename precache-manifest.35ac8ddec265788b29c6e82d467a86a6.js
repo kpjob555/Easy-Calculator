fbca4656981380461aa7ccafd3e75df92de94d58
MJDC <kpjob555@gmail.com>
2019-09-26 23:49:43 +0700

    Updates

---
+++ precache-manifest.35ac8ddec265788b29c6e82d467a86a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1784d2b638f25238e5ad202d4550c38",
+    "url": "/Easy-Calculator/index.html"
+  },
+  {
+    "revision": "a74e3e68885ce11ff71f",
+    "url": "/Easy-Calculator/static/css/main.0d9f44fa.chunk.css"
+  },
+  {
+    "revision": "343c1f5651f3c30c43f4",
+    "url": "/Easy-Calculator/static/js/2.ef5d389e.chunk.js"
+  },
+  {
+    "revision": "a74e3e68885ce11ff71f",
+    "url": "/Easy-Calculator/static/js/main.a8e66541.chunk.js"
+  },
+  {
+    "revision": "13d08127ed652952ace0",
+    "url": "/Easy-Calculator/static/js/runtime-main.abdb3d69.js"
+  }
+]);
